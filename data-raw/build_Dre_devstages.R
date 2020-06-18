edf5cdffcc88ea557c76ceef4d687148c2ffd9b9
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2020-06-18 15:50:31 +0200

    Added the build_devstage script

---
+++ data-raw/build_Dre_devstages.R
@@ -0,0 +1,20 @@
+datasets_s <- "name, tstart, tend, tunit
+Dre_emb_larval, 0, 1080, hours post-fertilization
+"
+
+
+devst <- read.table("data-raw/devstages.csv", h = T, sep = ',', stringsAsFactors = F)
+devst$tunit <- factor(devst$tunit)
+
+dss <- read.table(text = datasets_s, h = T, sep = ',', stringsAsFactors = F)
+dss$tunit <- factor(dss$tunit)
+
+Dre_devstages <- list(
+  devstages = devst, 
+  datasets = dss)
+
+save("Dre_devstages", file = "data/Dre_devstages.RData")
+
+rm(devstages_s, devst,
+   datasets_s, dss,
+   Dre_devstages)
