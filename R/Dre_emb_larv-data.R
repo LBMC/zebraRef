80596ce9924b399de5ff80964f21973b2e62345d
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2020-06-18 16:38:31 +0200

    Added doc for reference data

---
+++ R/Dre_emb_larv-data.R
@@ -0,0 +1,22 @@
+#' Dre_embryo
+#' 
+#' Transcriptomic data from *D. rerio* egg to larval samples.
+#' 
+#' One time series is included in this dataset.
+#'  - \insertCite{domazet2010phylogenetically}{zebraRef}
+#' 
+#' 
+#' The gene expression matrix is \eqn{log(X + 1)} of quantile-normalized expression data.
+#' 
+#' @docType data
+#' 
+#' @eval data_format()
+#' 
+#' @source \href{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE24616}{GSE24616}
+#' 
+#' @references
+#'    \insertAllCited{}
+#' 
+#' 
+#' @importFrom Rdpack reprompt
+"Dre_embryo"
