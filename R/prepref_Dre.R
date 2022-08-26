95a5a2725d0d245fe8dc9544d4cdcad28695639c
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2022-08-26 18:39:22 +0200

    Added function factory and applied to emb_larv reference

--- R/prepref_Dre.R
+++ R/prepref_Dre.R
@@ -3,35 +3,46 @@
 #' Builds the interpolation of the reference datasets.
 #' **These functions are internally called by \code{\link[RAPToR]{prepare_refdata}} from RAPToR.**
 #' 
-#' @param n.inter the resolution of the interpolation, as in \code{seq(start, end, length.out = n.inter)}.
+#' @param n.inter,by.inter the resolution of the interpolation, as in \code{seq(start, end, length.out = n.inter)} or \code{seq(start, end, by = by.inter)}.
 #' 
 #' @return A list with \code{interpGE} the interpolated gene expression matrix and 
 #' \code{time.series} the time of the interpGE matrix columns.
 #' 
-#' @seealso \code{\link[RAPToR]{prepare_refdata}} \code{\link[RAPToR]{ge_im}}
+#' @seealso \code{\link[RAPToR]{prepare_refdata}} \code{\link[RAPToR]{ge_im}} \code{\link[RAPToR]{make_ref}}
 #' 
 #' @name Dre_prep
 NULL
 
 #' @rdname Dre_prep
 #' @export
-#' @importFrom RAPToR ge_im
+#' @importFrom RAPToR ge_im make_ref
 #' @importFrom stats predict
 #' 
-.prepref_Dre_emb_larv <- function(n.inter){
-  # utils::data("Dre_emb_larv", envir = environment())
-  m <- RAPToR::ge_im(
-    X = zebraRef::Dre_emb_larv$g,
-    p = zebraRef::Dre_emb_larv$p,
-    formula = zebraRef::Dre_emb_larv$geim_params$formula,
-    method = zebraRef::Dre_emb_larv$geim_params$method,
-    dim_red = zebraRef::Dre_emb_larv$geim_params$dim_red,
-    nc = zebraRef::Dre_emb_larv$geim_params$nc
-  )
-  ndat <- data.frame(age = seq(min(zebraRef::Dre_emb_larv$p$age),
-                               max(zebraRef::Dre_emb_larv$p$age),
-                               l = n.inter))
-  return(
-    list(interpGE = predict(m, ndat), time.series = ndat$age)
-  )
+.prepref_skel <- function(data, from=NULL, to=NULL){
+  # .prepref function factory
+  f <- function(n.inter=NULL, by.inter=NULL){
+    m <- RAPToR::ge_im(
+      X = data$g,
+      p = data$p,
+      formula = data$geim_params$formula,
+      method = data$geim_params$method,
+      dim_red = data$geim_params$dim_red,
+      nc = data$geim_params$nc
+    )
+    return(RAPToR::make_ref(m, 
+                            n.inter = n.inter,
+                            by.inter = by.inter,
+                            from = from, 
+                            to = to,
+                            t.unit = data$t.unit,
+                            cov.levels = data$cov.levels,
+                            metadata = data$metadata)
+    )
+  }
+  return(f)
 }
+
+#' @rdname Dre_prep
+#' @export
+#' 
+.prepref_Dre_emb_larv <- .prepref_skel(zebraRef::Dre_emb_larv)
